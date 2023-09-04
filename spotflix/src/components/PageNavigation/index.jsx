faa4d8680174e8bc40ec453abaa59266852fe970
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-09-04 12:41:35 -0300

    att

--- spotflix/src/components/PageNavigation/index.jsx
+++ spotflix/src/components/PageNavigation/index.jsx
@@ -19,11 +19,13 @@ function ComponentPageNavigation({page, statePage}) {
         <PageNavigation>
             <FaAngleLeft onClick={() => {
                 page > 1 && statePage(prevPage => prevPage - 1)
+                window.scrollTo(0,0)
             }} />
 
             {page}
             <FaAngleRight onClick={() => {
                 page < 500 && statePage(prevPage => prevPage + 1)
+                window.scrollTo(0,0)
             }} />
         </PageNavigation>
     );
